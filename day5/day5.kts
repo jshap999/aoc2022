e37d746e2f2c8759a1b7cd10e1b0225cb76f370e
jshap999 <jshap999@yahoo.com>
2022-12-06 16:35:49 -0500

    aoc 2022 day 5

---
+++ day5/day5.kts
@@ -0,0 +1,95 @@
+#!/usr/bin/env kotlin
+
+import java.io.File
+
+data class Move(val count: Int, val fromIndex: Int, val toIndex: Int)
+
+if (args.size != 1) {
+    println("Please provide the file containing the puzzle input.")
+} else {
+    runCatching {
+
+        val puzzleInput = readFile(args.first())
+            .splitInput()
+
+        val boardInput = puszzleInput.first
+            .normalizeSpaces()
+            .splitRowsIntoColumns()
+
+        val board = setupBoard(boardInput)
+
+        val moves = puzzleInput.second
+            .toMoves()
+
+        moves.forEach { move ->
+            (0 until move.count).forEach {
+                val movingValue = board[move.fromIndex].removeFirst()
+                board[move.toIndex].add(0, movingValue)
+            }
+        }
+
+        val message = board.map { it.first() }.joinToString("")
+        println(message)
+
+    }.getOrElse {
+        println("Something went wrong.")
+        println("${it.stackTraceToString()}")
+    }
+}
+
+fun readFile(fileName: String): List<String> {
+    val file = File(fileName)
+
+    return file.readLines()
+        .map { if (it.isNullOrBlank()) null else it }
+        .filterNotNull()
+}
+
+fun List<String>.splitInput(): Pair<List<String>, List<String>> {
+    return this.fold(Pair(listOf(), listOf())) { end, item ->
+        when {
+            "^[ 0-9]+$".toRegex().matches(item) -> end
+            "^move .*".toRegex().matches(item) -> Pair(end.first, end.second.plus(item))
+            else -> Pair(end.first.plus(item), end.second)
+        }
+    }
+}
+
+fun List<String>.normalizeSpaces(): List<String> {
+    return this.map {
+        it.replace("    ".toRegex(), " [_] ")
+            .trim()
+            .replace("  +".toRegex(), " ")
+            .replace("\\[|\\]".toRegex(), "")
+    }
+}
+
+fun List<String>.splitRowsIntoColumns(): List<List<String>> {
+    return this.map {
+        it.split(" ")
+    }
+}
+
+fun setupBoard(input: List<List<String>>): List<MutableList<String>> {
+    val longestRow = input.fold(0) { longest, row ->
+        if (row.size > longest) row.size else longest
+    }
+
+    val board = List<MutableList<String>>(longestRow) { mutableListOf() }
+
+    input.forEach { row ->
+        row.forEachIndexed { col, item ->
+            if (item != "_") board[col].add(item)
+        }
+    }
+
+    return board
+}
+
+fun List<String>.toMoves(): List<Move> {
+    val regex = "move ([0-9]+) from ([0-9]+) to ([0-9]+)".toRegex()
+    return this.map {
+        val (_, count, from, to) = regex.matchEntire(it)?.groupValues ?: listOf(0, 0, 0, 0)
+        Move("$count".toInt(), "$from".toInt() - 1, "$to".toInt() - 1)
+    }
+}
